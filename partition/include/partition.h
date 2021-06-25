94e2781d8d7811825f1a326d23ffcec75d40410a
allangoes92 <57724201+allangoes92@users.noreply.github.com>
2021-06-25 20:37:28 -0300

    Add files via upload

---
+++ partition/include/partition.h
@@ -0,0 +1,33 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class ForwardIt, class UnaryPredicate>
+ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPredicate p)
+{
+    ForwardIt left = first;
+    while (first != last)
+    {
+        if (p(*first))
+            {
+                std::swap(*left, *first);
+                left++;
+            }
+        first++;
+    }
+    return left;
+}
+
+}
+#endif
