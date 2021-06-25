1193f63e968258583ea39bc18aa430cc92cfe46b
allangoes92 <57724201+allangoes92@users.noreply.github.com>
2021-06-25 20:30:12 -0300

    Add files via upload

---
+++ all_any_none_of/include/all_any_none_of.h
@@ -0,0 +1,72 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class UnaryPredicate>
+bool all_of(InputIt first, InputIt last, UnaryPredicate p)
+{
+    bool x = false;
+    while (first != last)
+    {
+        if(!p(*first))
+        {
+            return false;
+        }
+        else
+        {
+            x = true;
+        }
+        first++;
+    }
+    
+    return x;
+}
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class UnaryPredicate>
+bool any_of(InputIt first, InputIt last, UnaryPredicate p)
+{
+    while (first != last)
+    {
+        if(p(*first))
+        {
+            return true;
+        }
+        first++;
+    }
+    
+    return false;
+}
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class UnaryPredicate>
+bool none_of(InputIt first, InputIt last, UnaryPredicate p)
+{
+    while (first != last)
+    {
+        if(p(*first))
+        {
+            return false;
+        }
+        first++;
+    }
+    return true;
+}
+
+}
+#endif
